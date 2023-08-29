696e49cbfdba207e6a5966abe3687ea772ee72bb
Vipin <67505444+code240@users.noreply.github.com>
2023-08-29 23:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get("/api/sample",(req,res) => {
+    return [
+     "status" : true,
+        "data" : [
+        {
+            "name":"vipin",
+            "designation" : "software developer"
+        }
+        ]
+    ]
+})
 app.listen(process.env.PORT || 3000)
